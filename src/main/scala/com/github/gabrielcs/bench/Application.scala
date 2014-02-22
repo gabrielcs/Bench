b673711ac99e5f94ff80c7edb6c9e6780dfb63aa
Gabriel Cypriano <gabrielcs@gmail.com>
2014-02-22 19:50:47 -0300

    More idiomatic Scala error handling

--- src/main/scala/com/github/gabrielcs/bench/Application.scala
+++ src/main/scala/com/github/gabrielcs/bench/Application.scala
@@ -1,5 +1,9 @@
 package com.github.gabrielcs.bench
 
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
+
 object Application extends App {
   val ownerName = "10sheet"
   val repositoryName = "interview-repo"
@@ -10,19 +14,22 @@ object Application extends App {
   GithubRepositoryPrinter.printRepository(repository)
 
   def getDepth(args: Array[String]): Int = {
-    var depth = 0
-    try {
-      depth = augmentString(args(0)).toInt
-      if (depth < 1) {
-        println("Error: \"depth of the output\" should be 1 or higher")
-        System.exit(0)
-      }
-    } catch {
-      case e: RuntimeException =>
+    val depth = Try(args(0).toInt)
+    
+    depth match {
+      case Failure(thrown) => {
         println("Error: couldn't read \"depth of the output\"")
         System.exit(0)
+      }
+      case Success(s) => {
+        if (s < 1) {
+        	println("Error: \"depth of the output\" should be 1 or higher")
+        	System.exit(0)
+        }
+      }
     }
-    depth
+    
+    depth.get
   }
 }
 
